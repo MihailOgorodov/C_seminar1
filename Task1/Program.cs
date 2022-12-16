7e02ee6e0ba21a3479bd17b8c5631faa54a46732
“Mihail <mic311089@gmail.com>
2022-12-16 10:51:31 +0400

    Task1 complite

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите число: ");
+int a = int.Parse(ReadLine()!);
+
+WriteLine("Введите число: ");
+int b = int.Parse(ReadLine()!);
+
+if (a == b * b)
+{
+          WriteLine($"Число {a} является квадратом числа {b}");
+}
+else
+{
+          WriteLine($"Число {a} не является квадратом числа {b}");
+}
