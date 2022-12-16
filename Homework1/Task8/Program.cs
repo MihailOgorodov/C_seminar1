586c127caee3a992f2ba32797745581d81f2abd1
“Mihail <mic311089@gmail.com>
2022-12-16 13:13:14 +0400

    task8 complete

--- Homework1/Task8/Program.cs
+++ Homework1/Task8/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+using static System.Console;
+Clear();
+
+Write("Введите число: ");
+int a = int.Parse(ReadLine()!);
+int n = 1;
+
+while (n <= a)
+{
+          Write($" {n}");
+          n += 1;
+}
