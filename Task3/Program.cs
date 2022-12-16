fb657e8df522b22f17f0127acdf2dfac5d10d374
“Mihail <mic311089@gmail.com>
2022-12-16 11:06:13 +0400

    task3 complete

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите число от 1 до 7: ");
+int a = int.Parse(ReadLine()!);
+
+
+if (a == 1)
+{
+          WriteLine($"День недели - понедельник");
+}
+else if (a == 2)
+{
+          WriteLine($"День недели - вторник");
+}
+else if (a == 3)
+{
+          WriteLine($"День недели - среда");
+}
+else if (a == 4)
+{
+          WriteLine($"День недели - четверг");
+}
+else if (a == 5)
+{
+          WriteLine($"День недели - пятница");
+}
+else if (a == 6)
+{
+          WriteLine($"День недели - суббота");
+}
+else if (a == 7)
+{
+          WriteLine($"День недели - воскресенье");
+}
