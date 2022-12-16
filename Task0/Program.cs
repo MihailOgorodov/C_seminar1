02f0a337e26b39e19b0ebbac8d0b7bfd667b5ab3
“Mihail <mic311089@gmail.com>
2022-12-16 10:19:14 +0400

    Task0 complite

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите число: ");
+
+int a = int.Parse(ReadLine()!); // Преобразование сторчных данных в число
+// int b = Convert.ToInt32(ReadLine()); // Преобразование любых типов данных в число
+
+WriteLine($"Квадрат числа {a} равен {a*a}");
