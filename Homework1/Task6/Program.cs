79737213d009052782659ca08734d86ee0b13637
“Mihail <mic311089@gmail.com>
2022-12-16 13:06:56 +0400

    task6 complete

--- Homework1/Task6/Program.cs
+++ Homework1/Task6/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+using static System.Console;
+Clear();
+
+WriteLine("Введите число a: ");
+int a = int.Parse(ReadLine()!);
+
+if (a % 2 == 0) WriteLine($"Число {a} четное");
+else WriteLine($"Число {a} нечетное");
