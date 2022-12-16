21c2c826c25e34aaa22e6e6ae4113f01be57db3c
“Mihail <mic311089@gmail.com>
2022-12-16 12:58:30 +0400

    task4 complete

--- Homework1/Task4/Program.cs
+++ Homework1/Task4/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+using static System.Console;
+Clear();
+
+WriteLine("Введите число a: ");
+int a = int.Parse(ReadLine()!);
+
+WriteLine("Введите число b: ");
+int b = int.Parse(ReadLine()!);
+
+WriteLine("Введите число c: ");
+int c = int.Parse(ReadLine()!);
+
+if (a > b && a > c)
+{
+          WriteLine($"Максимальное число {a}");
+}
+else if (b > a && b > c)
+{
+          WriteLine($"Максимальное число {b}");
+}
+else
+{
+          WriteLine($"Максимальное число {c}");
+}
