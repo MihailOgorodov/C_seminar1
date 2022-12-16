0f1e0d4bf1a5e2c1edb6104102e607cb78f822b7
“Mihail <mic311089@gmail.com>
2022-12-16 12:45:31 +0400

    task2 complete

--- Homework1/Task2/Program.cs
+++ Homework1/Task2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+using static System.Console;
+Clear();
+
+WriteLine("Введите число a: ");
+int a = int.Parse(ReadLine()!);
+
+WriteLine("Введите число b: ");
+int b = int.Parse(ReadLine()!);
+
+if (a > b ) 
+{
+          WriteLine($"Число {a} больше числа {b}");
+}         
+else if (a < b)
+{          
+          WriteLine($"Число {b} больше числа {a}");
+}
+else
+{
+          WriteLine($"Числа равны");
+}
