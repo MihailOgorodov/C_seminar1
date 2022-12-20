ff8664ab5410ee81595cfdb9cb73fe6ad794835a
“Mihail <mic311089@gmail.com>
2022-12-20 10:59:19 +0400

    task11 added

---
+++ Task11/Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+using static System.Console;
+Clear();
+
+int number = new Random().Next(100, 1000);
+WriteLine(number);
+Write($"{number/100}{number%10}");
+
