d1b110c4159c42c2493f439add64e77b46b11d02
“Mihail <mic311089@gmail.com>
2022-12-23 11:45:34 +0400

    task20 lenght hypotenuse added

---
+++ Task20/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 2D пространстве.
+using static System.Console;
+Clear();
+
+Write("Введите координаты точки a по оси x: ");
+int.TryParse(ReadLine(), out int ax);
+
+Write("Введите координаты точки a по оси y: ");
+int.TryParse(ReadLine(), out int ay);
+
+Write("Введите координаты точки b по оси x: ");
+int.TryParse(ReadLine(), out int bx);
+
+Write("Введите координаты точки b по оси y: ");
+int.TryParse(ReadLine(), out int by);
+
+double gipotenusa = Math.Sqrt(Math.Pow((ax - bx), 2) + Math.Pow((ay - by), 2));
+
+
+WriteLine($"Расстояние между точками а и b равно {gipotenusa}");
