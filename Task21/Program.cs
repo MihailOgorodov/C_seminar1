820bd15846836ea8680574a3d4c46d3b193cdfcc
“Mihail <mic311089@gmail.com>
2022-12-23 12:19:58 +0400

    task21 complete

---
+++ Task21/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+using static System.Console;
+Clear();
+int i = 1;
+
+Write("Для вывода таблицы квадратов введите число: ");
+bool isParse = int.TryParse(ReadLine(), out int n);
+if (!isParse)
+{
+          WriteLine("Введено не число");
+          return;
+}
+WriteLine($"Таблица квадратов от 1 до {n}");
+
+while (i < n + 1)
+{
+          WriteLine($"Квадрат числа {i} = {Math.Pow(i, 2)}");
+          i++;
+}
