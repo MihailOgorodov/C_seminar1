53d23644a1447b72a6381b87070e9b636f8c1504
“Mihail <mic311089@gmail.com>
2022-12-24 11:31:07 +0400

    task23 added

---
+++ Homework3/Task23/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+using static System.Console;
+Clear();
+
+Write("Для вывода таблицы кубов введите число: ");
+bool isParse = int.TryParse(ReadLine(), out int n);
+if (!isParse)
+{
+          WriteLine("Введено не число");
+          return;
+}
+WriteLine($"Таблица кубов от 1 до {n}");
+int i = 1;
+while (i <= n)
+{
+          WriteLine($"Куб числа {i} = {Math.Pow(i, 3)}");
+          i++;
+}
