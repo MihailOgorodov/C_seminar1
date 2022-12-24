cfbffbce23567625a344a89d7fe9f3212523f61d
“Mihail <mic311089@gmail.com>
2022-12-24 11:27:01 +0400

    task21 added

---
+++ Homework3/Task21/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+using static System.Console;
+Clear();
+
+Write("Введите координаты точки a по оси x: ");
+int.TryParse(ReadLine(), out int ax);
+
+Write("Введите координаты точки a по оси y: ");
+int.TryParse(ReadLine(), out int ay);
+
+Write("Введите координаты точки a по оси z: ");
+int.TryParse(ReadLine(), out int az);
+
+Write("Введите координаты точки b по оси x: ");
+int.TryParse(ReadLine(), out int bx);
+
+Write("Введите координаты точки b по оси y: ");
+int.TryParse(ReadLine(), out int by);
+
+Write("Введите координаты точки b по оси z: ");
+int.TryParse(ReadLine(), out int bz);
+
+double hypotenuse = Math.Sqrt(Math.Pow((ax - bx), 2) + Math.Pow((ay - by), 2) + Math.Pow((az-bz), 2));
+
+
+WriteLine($"Расстояние между точками а и b в 3D пространстве равно {hypotenuse:f2}");
