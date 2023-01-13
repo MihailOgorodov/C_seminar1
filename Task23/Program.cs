ff06cf932fdf51864c8bcb221cf0ef57f184c9f7
“Mihail <mic311089@gmail.com>
2023-01-13 11:08:53 +0400

    task23 readded

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -7,29 +7,41 @@ int number = Convert.ToInt32(ReadLine());
 int sum = SumNumbers(number);
 WriteLine($"Количество числ в числе {number} = {sum}");
 
+// int SumNumbers(int A)
+// {
+//           int result = 0;
+//           if (A > 0 && A < 10)
+//           {
+//                     result = 1;
+//           }
+//           else if (A >= 10 && A < 100)
+//           {
+//                     result = 2;
+//           }
+//           else if (A >= 100 && A < 1000)
+//           {
+//                     result = 3;
+//           }
+//           else if (A >= 1000 && A < 10000)
+//           {
+//                     result = 4;
+//           }
+//           else if (A >= 10000 && A < 100000)
+//           {
+//                     result = 5;
+//           }
+//           return result;
+
+// }
+
 int SumNumbers(int A)
 {
-          int result = 0;
-          if (A > 0 && A < 10)
+          int digits = 0;
+          while (A > 0)
           {
-                    result = 1;
+                    A /= 10;
+                    digits++;
           }
-          else if (A >= 10 && A < 100)
-          {
-                    result = 2;
-          }
-          else if (A >= 100 && A < 1000)
-          {
-                    result = 3;
-          }
-          else if (A >= 1000 && A < 10000)
-          {
-                    result = 4;
-          }
-          else if (A >= 10000 && A < 100000)
-          {
-                    result = 5;
-          }
-          return result;
 
+          return digits;
 }
