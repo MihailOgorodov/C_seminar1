95e39b0d38a11ad9edf6184f30da6d884a013630
“Mihail <mic311089@gmail.com>
2023-01-13 10:47:43 +0400

    task24 complete

---
+++ Task24/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+using static System.Console;
+Clear();
+
+Write("Input number: ");
+int number = Convert.ToInt32(ReadLine());
+int multiply = GetMultiply (number);
+WriteLine($"Multiply  number from 1 to {number} = {multiply}");
+
+
+int GetMultiply (int A)
+{
+          int result = 1;
+          while (A > 0)
+          {
+                    result *= A;
+                    A--;
+          }
+
+          return result;
+}
