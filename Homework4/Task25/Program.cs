a6ca6a1c15edc0952b421ba414044569753b8c1c
“Mihail <mic311089@gmail.com>
2023-01-13 16:55:34 +0400

    task25 added

---
+++ Homework4/Task25/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 25: Напишите программу, реализующую метод,который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B с использованием цикла.
+using static System.Console;
+Clear();
+
+Write("Введите первое число: ");
+int A = Convert.ToInt32(ReadLine());
+Write("Введите второе число: ");
+int B = Convert.ToInt32(ReadLine());
+
+int expo = Exponent(A, B);
+WriteLine($"Число {A} в степени {B} = {expo}");
+
+int Exponent(int firstNum, int secondNum)
+{
+          int count = 1;
+          int result = 1;
+          if(secondNum < 0)
+          {
+                    secondNum *= -1; 
+          }
+          while (count <= secondNum)
+          {
+                    result *= firstNum;
+                    count++;                        
+          }
+          return result;
+}
