f1507cad250d82eb145778c38a8366916675dbeb
“Mihail <mic311089@gmail.com>
2023-01-17 10:50:22 +0400

    task28 added

---
+++ Task28_Inverse_Array/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 32: Напишите программу замены элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+using static System.Console;
+Clear();
+
+int[] Arr = GetArray(4);
+PrintArray(Arr);
+WriteLine();
+InverseArray(Arr);
+
+int[] GetArray(int size)
+{
+          int[] resultArray = new int[size];
+          for (int i = 0; i < resultArray.Length; i++)
+          {
+                    resultArray[i] = new Random().Next(-10, 10);
+          }
+          return resultArray;
+}
+
+void PrintArray(int[] inArray)
+{
+          Write("[");
+          for (int i = 0; i < inArray.Length - 1; i++)
+          {
+                    Write($"{inArray[i]},");
+          }
+          Write($"{inArray[inArray.Length - 1]}]");
+}
+
+void InverseArray(int[] inArray1)
+{
+          Write("[");
+          for (int i = 0; i < inArray1.Length - 1; i++)
+          {
+                    inArray1[i] *= -1;
+                    Write($"{inArray1[i]},");
+          }
+          Write($"{inArray1[inArray1.Length - 1]}]");
+}
+
