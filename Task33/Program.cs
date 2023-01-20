1bd636ac951e0e8952297275d8712263eabf6e61
“Mihail <mic311089@gmail.com>
2023-01-20 10:41:55 +0400

    task33 complete

---
+++ Task33/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+using static System.Console;
+Clear();
+Write("Введите 3 числа - 3 длины треугольника - через пробел: ");
+string[] triangleLenght = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+WriteLine(LengthOfTriangleSides(int.Parse(triangleLenght[0]), int.Parse(triangleLenght[1]), int.Parse(triangleLenght[2])) ? "Да, такой треугольник может быть" : "Нет, такого треугольника не может быть");
+
+bool LengthOfTriangleSides(int a, int b, int c)
+{
+          if (a + b > c && a + c > b && b + c > a) return true;
+          else return false;
+}
