b2bde6e930759988c0e93633727542b02e59ead2
“Mihail <mic311089@gmail.com>
2023-01-31 10:52:48 +0400

    task65 void method added

--- Seminar9/Task65/Program.cs
+++ Seminar9/Task65/Program.cs
@@ -8,8 +8,19 @@ Write("Введите число M и N через пробел: ");
 int[] parameters = Array.ConvertAll(ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries), Convert.ToInt32);
 
 WriteLine($"Все натуральные числа в промежутке от {parameters[0]} до {parameters[1]} -> {GetNumbers(parameters[0], parameters[1])}");
+PrintNumbers(parameters[0], parameters[1]);
 
 string GetNumbers(int minValue, int maxValue)
 {
-          return (minValue == maxValue)?maxValue.ToString():$"{GetNumbers(minValue, maxValue-1)}, {maxValue}";
+          return (maxValue == minValue)?maxValue.ToString():$"{GetNumbers(minValue, maxValue-1)}, {maxValue}";
+}
+
+void PrintNumbers(int minValue1, int maxValue1)
+{
+          if(maxValue1 == minValue1) Write(maxValue1);
+          else
+          {
+                    PrintNumbers(minValue1, maxValue1 -1);
+                    Write($", {maxValue1}");
+          }
 }
