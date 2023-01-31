f69f946d27c506934652b5351a8904c335f3bc23
“Mihail <mic311089@gmail.com>
2023-01-31 14:38:39 +0400

    task67 added

---
+++ Seminar9/Task67/Program.cs
@@ -0,0 +1,15 @@
+﻿// **Задача 67:** Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+using static System.Console;
+Clear();
+
+Write("Введите число: ");
+int number = Convert.ToInt32(ReadLine());
+WriteLine(SumElements(number));
+
+int SumElements(int num)
+{
+          if (num == 0) return num;
+          else return SumElements(num/10) + num%10;
+}
